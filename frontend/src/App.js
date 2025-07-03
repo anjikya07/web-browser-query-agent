fb838ef230e5c93937ccffd57d762b472dbcfd58
@njiky@ <anjikyashrivas07@gmail.com>
2025-07-03 13:10:29 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import './App.css';
+import QueryForm from './components/QueryForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Web Query Agent</h1>
+      <QueryForm />
+    </div>
+  );
+}
+
+export default App;
